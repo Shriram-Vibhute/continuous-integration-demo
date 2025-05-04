b47811b0859af8769e66fc1fb15239b79f48c385
Shriram-Vibhute <shriram.vibhute08@gmail.com>
2025-05-04 17:55:34 +0530

    Fix formatting by removing unnecessary newlines in app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
     return a - b
+
 
 val1 = add(1, 2)
 val2 = subtract(5, 3)
 
-print(val1, val2)
-
-# flake8 app.py -> You can check your code follows PEP8 guide lines. 
+print(val1, val2)# flake8 app.py -> You can check your code follows PEP8 guide lines. 
