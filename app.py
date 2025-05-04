91bcf565758b51d68e36c666f9f5d8cc878b44fe
Shriram-Vibhute <shriram.vibhute08@gmail.com>
2025-05-04 17:54:14 +0530

    Refactor formatting for consistency and readability in app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-def add(a,b):
+def add(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
 
-
-
-def subtract(a,b):return a-b
-
-val1 = add(1,2)
-val2 = subtract(5,3)
+val1 = add(1, 2)
+val2 = subtract(5, 3)
 
 print(val1, val2)
 
