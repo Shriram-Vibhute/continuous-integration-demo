dc27e0b5b68fa9486e4a38ded05af3a1f6f75861
Shriram-Vibhute <shriram.vibhute08@gmail.com>
2025-05-04 21:38:30 +0530

    Refactor test_app.py to use consistent function names for clarity

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 # test_app.py
 
 import unittest
-from app import add, sub, mul
+from app import add, subtract, multiply
 
 class TestMathFunctions(unittest.TestCase):
 
@@ -10,12 +10,12 @@ class TestMathFunctions(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
 
     def test_sub(self):
-        self.assertEqual(sub(4, 5), -1)
-        self.assertEqual(sub(-1, -1), 0)
+        self.assertEqual(subtract(4, 5), -1)
+        self.assertEqual(subtract(-1, -1), 0)
 
     def test_mul(self):
-        self.assertEqual(mul(4, 5), 20)
-        self.assertEqual(mul(-1, -1), 1)
+        self.assertEqual(multiply(4, 5), 20)
+        self.assertEqual(multiply(-1, -1), 1)
 
 if __name__ == '__main__':
     unittest.main()
